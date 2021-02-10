5b04edc9cb07828e257c19b81a779318a73b7bf7
Anton Vasilev <antony.n.vasilev@gmail.com>
2021-02-10 15:30:42 +0300

    updated task_5.py

--- task_5.py
+++ task_5.py
@@ -5,5 +5,5 @@
 from string import ascii_lowercase
 
 letters = ascii_lowercase
-a = input('Введите букву: ')
-print(f'Буква {a} - {letters.find(a) + 1}-я буква в алфавите.')
+a = int(input('Введите номер буквы: '))
+print(f'{a}-я буква в алфавите - {letters[a - 1]}.')
