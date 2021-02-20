6e4aee65d767507ae11d976ab1bbf6dcea844a9a
Antony Vasilev <antony.n.vasilev@gmail.com>
2021-02-20 13:37:25 +0300

    added task 1

---
+++ task_1.py
@@ -0,0 +1,35 @@
+"""
+Пользователь вводит данные о количестве предприятий, их наименования и прибыль за четыре квартала для каждого
+предприятия. Программа должна определить среднюю прибыль (за год для всех предприятий) и отдельно вывести наименования
+предприятий, чья прибыль выше среднего и ниже среднего.
+"""
+
+from collections import namedtuple
+
+Companies = namedtuple('Companies', ['name', 'profit'])
+companies_list = []
+avg_profit = 0
+less_avg = []
+more_avg = []
+
+n = int(input('Введите количество предприятий: '))
+
+for i in range(n):
+    name = input(f'Введите наименование {i + 1}-го предприятия: ')
+    profit = float(input(f'Введите прибыль {i + 1}-го предприятия за 4 квартала: '))
+    companies_list.append(Companies(name=name, profit=profit))
+    avg_profit += profit
+
+avg_profit /= n
+
+for i in range(n):
+    if companies_list[i].profit > avg_profit:
+        more_avg.append(companies_list[i].name)
+    elif companies_list[i].profit < avg_profit:
+        less_avg.append(companies_list[i].name)
+    # Варианта равного средней прибыли в условии нет, поэтому оставляю так.
+
+print(f'Средняя прибыль: {avg_profit}')
+print(f'Список компаний с прибылью больше средней: {more_avg}')
+print(f'Список компаний с прибылью меньше средней: {less_avg}')
+
